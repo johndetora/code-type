0b8056b48a24b6d1504e90cd2a2f7d6b0dbe4f3a
johndetora <39231237+johndetora@users.noreply.github.com>
2021-04-15 19:47:09 -0400

    Add files via upload
    initial commit

---
+++ phrases.js
@@ -0,0 +1,16 @@
+// Here is the library of phrases.
+// To do: organize into seperate languages function types
+export const phrases = [
+    // HTML
+    '<link rel="stylesheet" href="./styles/index.css"></link>',
+    '<script src="./script.js">',
+    '<input type="range" class="parameter" value=0 min=0 max=7 step=1>',
+    // JS
+    "const element = document.querySelectorAll('.class');",
+    'parseFloat(value).toFixed(2);',
+    'for (let i = 0; i < array.length; i++) {',
+    'let vals = [4, 8, 1, 2, 9, 6];',
+    "document.addEventListener('click', ({target}) => {",
+    "const createSpan = document.createElement('span');",
+    'vals = vals.map(x => x * 2);',
+];
