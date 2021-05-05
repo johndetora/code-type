4a7bc6fb5b7441c51a84f0ccd695bdd5222869a9
John DeTora <johndetora@gmail.com>
2021-05-04 20:32:41 -0400

    new phrases

--- phrases.js
+++ phrases.js
@@ -7,17 +7,19 @@ export const phrases = [
     '<script src="./script.js">',
     '<input type="range" class="parameter" value=0 min=0 max=7 step=1>',
     // js
+    'const log = console.log;',
     "const element = document.querySelectorAll('.class');",
     'parseFloat(value).toFixed(2);',
     'for (let i = 0; i < array.length; i++) {',
     'let vals = [4, 8, 1, 2, 9, 6];',
     "document.addEventListener('click', ({target}) => {",
     "const createSpan = document.createElement('span');",
-    // modern js
+    // arrow functions
     'vals = vals.map(x => x * 2);',
     'array.reduce((accumulator, person) => accumulator + person.age), 0);',
     'const result = words.filter(word => word.length > 6);',
-    'evenNumbers = arr.filter(x => x % 2 ==1);',
+    'evenNumbers = arr.filter(x => !(x % 2));',
+    "arr.filter((x) => typeof x == 'string');",
     // CSS
     'grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));',
 ];
